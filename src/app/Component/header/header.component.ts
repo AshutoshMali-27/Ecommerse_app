e128c37bdcd741ac8d851ad3314d94dd80d408e9
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-10-06 00:55:07 +0530

    register component commit

--- src/app/Component/header/header.component.ts
+++ src/app/Component/header/header.component.ts
@@ -1,5 +1,7 @@
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 import { NavigationItem } from './../../Models/models';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,Type,ViewChild,ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('modalTitle') modelTitle !: ElementRef;
+  @ViewChild('container' ,{read: ViewContainerRef ,static:true})
+  container !:ViewContainerRef
 navigationList : NavigationItem[]=[
 
 
@@ -24,7 +29,23 @@ navigationList : NavigationItem[]=[
 
   constructor(){}
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void { }
+
+  openModal(name: string){
+    this.container.clear();
+
+    let componentType!: Type<any>;
+    if (name === 'login') {
+      componentType = LoginComponent;
+      this.modelTitle.nativeElement.textContent = 'Enter Login Information';
+    }
+    if (name === 'register') {
+      componentType = RegisterComponent;
+      this.modelTitle.nativeElement.textContent = 'Enter Register Information';
+    }
+
+    this.container.createComponent(componentType);
   }
+
+  
 }
