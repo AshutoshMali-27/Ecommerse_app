08c6bd8778001c5b1487c34f95ec402f0234bf57
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-10-07 14:19:06 +0530

    commit3

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,
+  FormGroup,
+   ReactiveFormsModule
+   , Validators
+   ,FormControl
+   
+  } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginFrom!: FormGroup;
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+this.loginFrom =this.fb.group({
+  email:['',[Validators.required,Validators.email]],
+  pwd:[
+    '',[
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+    ],
+  ],
+});
+  }
+login(){}
+
+get Email(): FormControl{
+  return this.loginFrom.get('email') as FormControl
+}
+
+get Password(): FormControl{
+  return this.loginFrom.get('pwd') as FormControl
+}
+
+  
 
 }
